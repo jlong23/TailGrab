6f39249721dbe8920cf25b43695e4b3c3ec1fb5c
Jay Long <jlong@rabbitearsvideoproduction.mobi>
2026-02-06 12:04:44 -0600

    Update Database Migrations

--- Migrations/20260206180413_V1.0.10.Designer.cs
+++ Migrations/20260206180413_V1.0.10.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tailgrab.Models;
 
@@ -10,9 +11,11 @@ using Tailgrab.Models;
 namespace tailgrab.Migrations
 {
     [DbContext(typeof(TailgrabDBContext))]
-    partial class TailgrabDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260206180413_V1.0.10")]
+    partial class V1010
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
